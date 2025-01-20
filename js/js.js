108a0d02847f56dc19b5e571899ab27c8b505aa8
alina m <alinamocalova451@gmail.com>
2025-01-20 22:06:14 +0300

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,18 @@
+//задача 1
+let str='abcde';
+alert(str[0]);
+alert(str[1]);
+alert(str[4]);
+
+//задача 2
+let x=parseInt(prompt("введите первое число:"));
+let y=parseInt(prompt("введите второе число:"));
+console.log( x>0 || y>0);
+
+//задача 3
+let a=parseInt(prompt("введите свой возраст:"));
+if (a>=18){
+    console.log('Совершеннолетний');
+} else {
+    console.log('Несовершеннолетний');
+}
